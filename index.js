58ed22f26d9b3dc34efca1f7c12076344c43a1ae
Sam <sammyliebman@gmail.com>
2018-04-17 01:06:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,16 @@ function updateObjectWithKeyAndValue(object, key, value){
   object[key] = value
   return object
 }
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value
+  return object
+}
+
+function deleteFromObjectByKey(object, key) {
+  
+}
+
+function destructivelyDeleteFromObjectByKey(object, key){
+  
+}
